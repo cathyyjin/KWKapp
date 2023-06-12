ed08d4ec7ef5a34aa9388510dc8f7042bee80ce9
Cathy Jin <jincathy3@gmail.com>
2023-06-12 14:27:33 -0500

    Initial Commit

---
+++ newgithub/newgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  newgithubApp.swift
+//  newgithub
+//
+//  Created by Cathy Jin on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct newgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
