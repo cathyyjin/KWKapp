0f24c766d8db51062e32b9dbfb88e6284297fd52
Cathy Jin <jincathy3@gmail.com>
2023-06-14 22:36:48 -0500

    searchbar

--- newgithub/ResourcePage.swift
+++ newgithub/ResourcePage.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct ResourcePage: View {
+    private var listOfsubject = subjectList
+    @State var searchText = ""
+    
     var body: some View {
-        Text("This is Resource Page")
+        NavigationView{
+            List{
+                ForEach(subjects, id: \.self) { subject in
+                    HStack{
+                        Text(subject.capitalized)
+                        Spacer()
+                        Image(systemName: "book.fill")
+                    }
+                    .padding()
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle ("help:")
+           
+            
+            
+        }
+        
+        
+    }
+    var subjects: [String]{
+        let lcSubjects = listOfsubject.map { $0.lowercased() }
+        
+        return searchText == "" ? lcSubjects : lcSubjects.filter {
+            $0.contains(searchText.lowercased())
+        }
     }
 }
 
---
+++ newgithub/Subjects.swift
@@ -0,0 +1,10 @@
+//
+//  Subjects.swift
+//  newgithub
+//
+//  Created by Cathy Jin on 6/14/23.
+//
+
+import Foundation
+
+public var subjectList = ["Biology","Calculus BC","Chemistry","Computer Science A","English Language and Composition","English Literature and Composition","Government and Politics (US)","Human Geography","Macroeconomics","Microeconomics","Physics 1: Algebra-Based","Physics 2: Algebra-Based","Physics C: Electricity and Magnetism","Physics C: Mechanics","Statistics","US History","World History: Modern"]
