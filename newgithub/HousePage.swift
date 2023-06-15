a2ee952d98f35c3743a6302db658634b9fb887ba
Valerie Mondragon <mondragonva7@gmail.com>
2023-06-15 12:33:39 -0700

    House Page Logo

--- newgithub/HousePage.swift
+++ newgithub/HousePage.swift
@@ -9,21 +9,33 @@ import SwiftUI
 
 struct HousePage: View {
     var body: some View {
+        
+        
         ZStack {
             Image("Sage-background")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(minWidth: 0, maxWidth: .infinity)
                 .edgesIgnoringSafeArea(.all)
+            
+            
             VStack {
                 
-                Image("Title").aspectRatio(contentMode: .fit).frame(width: -5.0) .offset(y: -200)
+                Image("LOGO 1")
+                   // .offset(y: -250)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 800, height: 300) .offset(y:-300)
+                    .blur(radius: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
                 
-                Text("Don't Ignore Your Own potential") .offset(y:-225)
-                    .font(.subheadline)
+                Text("Don't Ignore Your Own potential")
+                    .lineLimit(2)
+                    .offset(y:-350)
+                    .font(.title3)
                     .fontWeight(.semibold)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
+                    .blur(radius: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
                 
                 
             }
