d98b9fa9df90d21ee927eb0d953558a0c0ec81f5
Ashley <ashleyramirezgabri@gmail.com>
2023-06-15 07:28:58 -0700

    Just for right now

--- newgithub/PhotoPage.swift
+++ newgithub/PhotoPage.swift
@@ -4,17 +4,55 @@
 //
 //  Created by Cathy Jin on 6/13/23.
 //
-
 import SwiftUI
 
 struct PhotoPage: View {
+    
+    
+    
+    @State var isImagePickerShowing = false
+    @State var selectedImage: UIImage?
+    @State private var sourceType:
+    UIImagePickerController.SourceType = .photoLibrary
+    
+    
     var body: some View {
-        Text("This is photo page")
+        
+        VStack {
+            Image(uiImage: selectedImage ??
+                  UIImage(named: "camera")!)
+            .resizable ()
+            .aspectRatio(contentMode: .fit)
+            
+            
+            HStack {
+                
+                Button ("Select a Photo"){
+                    self.sourceType = .photoLibrary
+                    isImagePickerShowing = true
+                    
+                }
+                .padding ()
+                
+                Button ("Take a Photo") {
+                    self.sourceType = .camera
+                    isImagePickerShowing = true
+                
+                }
+                .padding ()
+                
+            }
+        }
+        .sheet(isPresented: $isImagePickerShowing){
+            ImagePicker(selectedImage: $selectedImage, isImagePickerShowing: $isImagePickerShowing, sourceType: self.sourceType)
+        }
     }
 }
 
 struct PhotoPage_Previews: PreviewProvider {
     static var previews: some View {
-        PhotoPage()
+        ContentView()
     }
 }
+
+    
