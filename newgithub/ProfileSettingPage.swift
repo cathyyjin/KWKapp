db1fc2347335899ffd7661a0ba8124fefe047d67
Cathy Jin <jincathy3@gmail.com>
2023-06-15 22:36:25 -0500

    notecards study tip

--- newgithub/ProfileSettingPage.swift
+++ newgithub/ProfileSettingPage.swift
@@ -16,22 +16,28 @@ struct ProfileSettingPage: View {
             Text("\(SavedName)")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .offset(y: 20)
             Spacer()
             
-            Image("profile")
+            Image("smart")
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .offset(y: -100)
             
-            Text("Hello")
+            Text("Hello Scholar")
                 .font(.title)
                 .fontWeight(.semibold)
-                .offset(y: -5)
+                .offset(y: -80)
             
                  TextField("Enter Your Name", text: $username)
                 .font(.title)
-            
+                .offset(y: -80)
                  Button (action: {saveData()}){
-                Text("Save")
+                     Text("Save")
+                         .font(.title3)
+                     
+                         .offset(y: -60)
             }
                  Spacer()
             
--- newgithub/StudyTipPage.swift
+++ newgithub/StudyTipPage.swift
@@ -9,10 +9,55 @@ import SwiftUI
 
 struct StudyTipPage: View {
     var body: some View {
-        Text("Study Tip Page!")
+        VStack {
+            Text("Study Tips")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            List {
+                StudyTipCard(imageName: "", title: "Pomodoro Method", description: "- Intervals of 25 minute intense work sessions with 5 minute breaks.              - Increases Focus and Spreads Productivy levels throughout the day")
+                
+                StudyTipCard(imageName: "", title: "Feynman Technique", description: "1. Select a concept to learn            2.Teach it to a child: simplicity reveals a depth of understanding           3.Review and refine your understanding: writing helps reflection and learning.           4.Organize your notes and revisit them regularly.")
+                
+                StudyTipCard(imageName: "", title: "Active Recall", description: "Instead of copying yoru notes, close your books and try recalling the main ideas and explain them in your own words. Then, write questions for yourself on the topic to test yourself")
+                
+                StudyTipCard(imageName: "", title: "", description: ".")
+                
+                StudyTipCard(imageName: "", title: "", description: "")
+            }
+            .listStyle(PlainListStyle())
+            .padding(.horizontal)
+        }
     }
 }
-
+struct StudyTipCard: View {
+    var imageName: String
+    var title: String
+    var description: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 200)
+                .cornerRadius(10)
+            
+            Text(title)
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Text(description)
+                .font(.body)
+                .multilineTextAlignment(.center)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+    }
+}
 struct StudyTipPage_Previews: PreviewProvider {
     static var previews: some View {
         StudyTipPage()
